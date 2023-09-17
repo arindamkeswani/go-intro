cb3d82096cce8adb976f1ccda27685dcfab8317a
Arindam Keswani PW <arindam.keswani@pw.live>
2023-09-17 20:46:28 +0530

    feat(main): initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+//1. Init project by creating the module with the cmd "go mod init booking-app"
+// This will generate the go.mod file
+
+//2. Every file must belong to a package, which should be defined in the first line
+
+//4. Packages are simply a collection of source files, we are importing one of those, fmt, here
+//5. You can run the project using the cmd "go run main.go", this compiles and runs the code
+import (
+	"fmt"
+)
+
+// 3. This function is the entrypoint of the project
+func main() {
+
+	fmt.Println("Welcome to our conference booking application");
+    fmt.Println("You can book your tickets here!");
+}
