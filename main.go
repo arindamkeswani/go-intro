2a0804d3114117ee7c6b7d8c7800768692cbbccc
Arindam Keswani PW <arindam.keswani@pw.live>
2023-09-21 23:33:42 +0530

    User Input Validation

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ package main
 //4. Packages are simply a collection of source files, we are importing one of those, fmt, here
 //5. You can run the project using the cmd "go run main.go", this compiles and runs the code
 import (
-	"fmt";
-    "strings";
+	"fmt"
+	"strings"
 )
 
 // 3. This function is the entrypoint of the project
@@ -21,9 +21,9 @@ func main() {
 	fmt.Printf("Welcome to %s booking application\n", conferenceName)
 	fmt.Println("You can book your tickets here!")
 	fmt.Printf("We have %v out of %v tickets remaining\n", remainingTickets, conferenceTicketCount)
-    var bookings []string // This is a slice
+	var bookings []string // This is a slice
 
-	for remainingTickets > 0 && len(bookings) < 50{
+	for remainingTickets > 0 && len(bookings) < 50 {
 
 		// var bookings [50]string; //This is an array declaration
 		// Data Types can either be inferred directly if they are assigned, or they can be explicitly defined
@@ -50,44 +50,60 @@ func main() {
 		fmt.Println("Please enter the number of tickets that you would like to book: ")
 		fmt.Scan(&userRequestedTicketCount)
 
-        if userRequestedTicketCount > remainingTickets {
-            fmt.Printf("We only have %v tickets remaining!\n", remainingTickets);
-            continue;
-        } else if userRequestedTicketCount == remainingTickets {
-            fmt.Printf("Those were the last of our tickets, see you at the conference!");
-        }
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNo := userRequestedTicketCount > 0 && userRequestedTicketCount <= remainingTickets
+		// isValidCity := city == "Singapore" || city == "London";
 
-		remainingTickets = remainingTickets - userRequestedTicketCount
-		// bookings[0] = firstName + " " + lastName; //Syntax for assigning an array index
-		bookings = append(bookings, firstName+" "+lastName) // Syntax for appending an elem in the last place in a slice
+		if !isValidName || !isValidEmail || !isValidTicketNo {
+			// fmt.Printf("We only have %v tickets remaining!\n", remainingTickets);
+            if !isValidName {
+                fmt.Println("Name is too short");
+            }
+            if !isValidEmail {
+                fmt.Println("Invalid email");
+            }
+            if !isValidTicketNo {
+                fmt.Println("Invalid number of tickets selected");
+            }
+			fmt.Printf("Invalid input, try again.\n")
+			continue;
+		} else if userRequestedTicketCount == remainingTickets {
+			fmt.Printf("Those were the last of our tickets, see you at the conference!")
+		} else {
 
-		// fmt.Printf("The whole slice: %v\n", bookings)
-		// fmt.Printf("The first value: %v\n", bookings[0])
-		// fmt.Printf("Slice type: %T\n", bookings)
-		// fmt.Printf("Slice length: %v\n", len(bookings))
+			remainingTickets = remainingTickets - userRequestedTicketCount
+			// bookings[0] = firstName + " " + lastName; //Syntax for assigning an array index
+			bookings = append(bookings, firstName+" "+lastName) // Syntax for appending an elem in the last place in a slice
 
-        firstNames := []string{};
-    
-        for _, booking := range bookings { //_ is used to define a variable that we wish to ignore
-            var names = strings.Fields(booking); //splits the str passed in the param using a whitespace
-            var firstName = names[0];
-            firstNames = append(firstNames, firstName);
-        }
+			// fmt.Printf("The whole slice: %v\n", bookings)
+			// fmt.Printf("The first value: %v\n", bookings[0])
+			// fmt.Printf("Slice type: %T\n", bookings)
+			// fmt.Printf("Slice length: %v\n", len(bookings))
 
-		// fmt.Printf("These are our bookings: %v\n", bookings)
-        fmt.Printf("Bookings (first names) are: %v\n",firstNames);
+			firstNames := []string{}
 
-		// fmt.Printf("Thank you for booking %v ticket(s), %v %v. You will receive a confirmation mail at %v\n", userRequestedTicketCount, firstName, lastName, email)
-		// fmt.Printf("conferenceTicketCount is of the Type: %T, conferenceName is of the Type: %T",conferenceTicketCount, conferenceName);
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-       
-        var noTicketsRemaining bool = remainingTickets <= 0;
-        if  noTicketsRemaining {
-            //end program
-            fmt.Println("Sold Out! Come back next year!\n");
-            break;
-        }
-        
+			for _, booking := range bookings { //_ is used to define a variable that we wish to ignore
+				var names = strings.Fields(booking) //splits the str passed in the param using a whitespace
+				var firstName = names[0]
+				firstNames = append(firstNames, firstName)
+			}
+
+			// fmt.Printf("These are our bookings: %v\n", bookings)
+			fmt.Printf("Bookings (first names) are: %v\n", firstNames)
+
+			// fmt.Printf("Thank you for booking %v ticket(s), %v %v. You will receive a confirmation mail at %v\n", userRequestedTicketCount, firstName, lastName, email)
+			// fmt.Printf("conferenceTicketCount is of the Type: %T, conferenceName is of the Type: %T",conferenceTicketCount, conferenceName);
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			var noTicketsRemaining bool = remainingTickets <= 0
+			if noTicketsRemaining {
+				//end program
+				fmt.Println("Sold Out! Come back next year!\n")
+				break
+			}
+
+		}
 
 	}
 }
